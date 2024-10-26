e2ca06746883aab815527dbd83ad2e373057fbf0
Kenjiro56 <hiroken506@gmail.com>
2024-10-26 11:38:51 +0900

    feat: 返り値の型指定

--- backend/nextConnect/nextConnect.go
+++ backend/nextConnect/nextConnect.go
@@ -4,11 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func nextConnect() {
-	router := gin.Default()
+func NextConnect() *gin.Engine {
+	connector := gin.Default()
 
 	// cors設定
-	router.Use(func(cors *gin.Context) {
+	connector.Use(func(cors *gin.Context) {
 		cors.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")       // 一旦ローカルホストに設定(本番は環境変数で設定するのが良さそう)
 		cors.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS") // 一旦GET, POST, DELETEのみ許可
 		cors.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
@@ -20,9 +20,12 @@ func nextConnect() {
 		cors.Next()
 	})
 
-	router.GET("/test", func(c *gin.Context) {
+	// test用のエンドポイント
+	connector.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello, World!!!waiwai2",
 		})
 	})
+
+	return connector
 }
