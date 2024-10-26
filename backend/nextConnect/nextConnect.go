70813f853429339f943754160dbc31f28048d75b
Kenjiro56 <hiroken506@gmail.com>
2024-10-26 11:03:45 +0900

    feat: test用のレスポンス作成

--- backend/nextConnect/nextConnect.go
+++ backend/nextConnect/nextConnect.go
@@ -5,12 +5,11 @@ import (
 )
 
 func nextConnect() {
-
 	router := gin.Default()
 
 	// cors設定
 	router.Use(func(cors *gin.Context) {
-		cors.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")       // 一旦ローカルホストに設定(本番は環境変数で設定するのが良さそう)
+		cors.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")       // 一旦ローカルホストに設定(本番は環境変数で設定するのが良さそう)
 		cors.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS") // 一旦GET, POST, DELETEのみ許可
 		cors.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 		cors.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
@@ -19,5 +18,11 @@ func nextConnect() {
 			return
 		}
 		cors.Next()
+	})
+
+	router.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!!!waiwai2",
+		})
 	})
 }
