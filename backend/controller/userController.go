b310b150980aee6fa8261a2797f78d4c266acb56
KENKUN-1031 <kencoding1031@gmail.com>
2024-10-26 14:29:16 +0900

    feat: :sparkles: backendの全ユーザー&特定のユーザー取得機能

--- backend/controller/userController.go
+++ backend/controller/userController.go
@@ -3,22 +3,38 @@ package controller
 import (
 	"KansaiHack-Friday/db"
 	"KansaiHack-Friday/models"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func CreateUser(c *gin.Context) {
+func CreateUser(c *gin.Context) { //ユーザーの作成
 	var user models.User
 	if err := c.ShouldBindJSON(&user); err != nil {
-		fmt.Printf("----------------")
-		fmt.Printf("User名：", user)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	if err := db.DB.Create(&user).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func GetUsers(c *gin.Context) { //全ユーザーの取得
+	var users []models.User
+	if err := db.DB.Find(&users).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, users)
+}
+
+func GetUser(c *gin.Context) { //ユーザーの検索
+	id := c.Param("id")
+	var user models.User
+	if err := db.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 		return
 	}
 	c.JSON(http.StatusOK, user)
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,19 +2,16 @@ package routes
 
 import (
 	"KansaiHack-Friday/controller"
-	"KansaiHack-Friday/handlers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func DefineRoutes(r gin.IRouter) {
 
-	database := r.Group("/db")
-	{
-		database.GET("/", handlers.CheckDBConnection)
-	}
 	auth := r.Group("/")
 	{
 		auth.POST("/signup", controller.CreateUser)
+		auth.GET("/allusers", controller.GetUsers)
+		auth.GET("/user/:id", controller.GetUser)
 	}
 }
