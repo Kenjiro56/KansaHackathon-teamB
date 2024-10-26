4d779194476550e2c000fe9f547f732cddb44ce1
Kenjiro56 <hiroken506@gmail.com>
2024-10-26 10:37:32 +0900

    feat: corsの設定

--- backend/nextConnect.go
+++ backend/nextConnect.go
@@ -1,1 +1,18 @@
 package nextConnect
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// cors設定
+router.Use(func(cors *gin.Context) {
+		cors.Header("Access-Control-Allow-Origin", "http://localhost:8080") // 一旦ローカルホストに設定(本番は環境変数で設定するのが良さそう)
+		cors.Writer("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS") // 一旦GET, POST, DELETEのみ許可
+		cors.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+		cors.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		if cors.Request.Method == "OPTIONS" {
+			cors.AbortWithStatus(200)
+			return
+		}
+		cors.Next()
+})
