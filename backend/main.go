f1ae43e8ca8046e6a1c5b5d1a337c2b3a15588b8
Kenjiro56 <hiroken506@gmail.com>
2024-10-26 11:39:23 +0900

    feat: test用に変更

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"KansaiHack-Friday/nextConnect"
 )
 
 func main() {
+	router := nextConnect.NextConnect()
 	//Ginフレームワークのデフォルトの設定を使用してルータを作成
-	router := gin.Default()
+	// router := gin.Default()
 
-	// ルートハンドラの定義
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, World!!!waiwai2",
-		})
-	})
+	// // ルートハンドラの定義
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "Hello, World!!!waiwai2",
+	// 	})
+	// })
 
 	// サーバー起動
 	router.Run(":8080")
