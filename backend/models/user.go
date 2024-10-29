30f8d3a8898ce60dc67895758afedaf0d4efd127
KENKUN-1031 <kencoding1031@gmail.com>
2024-10-29 11:47:36 +0900

    refactor: :art: user追加に軽いvalidationを追加

--- backend/models/user.go
+++ backend/models/user.go
@@ -5,8 +5,8 @@ import "time"
 type User struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
 	UserName  string    `gorm:"size:255;not null" json:"user_name"`
-	Email     string    `gorm:"size:255;unique;not null" json:"email"`
-	Password  string    `gorm:"size:255;not null" json:"password"`
+	Email     string    `gorm:"size:255;unique;not null" json:"email" binding:"required,email"`
+	Password  string    `gorm:"size:255;not null" json:"password" binding:"required,min=8"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
