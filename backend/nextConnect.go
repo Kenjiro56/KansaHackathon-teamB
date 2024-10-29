7fc6477fcb8aedda65ffb54f430483251afe05fd
Mami Tsutae <sora.teru.96neko@gmail.com>
2024-10-29 16:49:58 +0900

    feature: .ignoreファイルの追加

--- backend/nextConnect.go
+++ backend/nextConnect.go
@@ -1,1 +1,11 @@
-package nextConnect
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+func NextConnect(c *gin.Context) {
+    // 関数の実装
+    c.JSON(200, gin.H{
+        "message": "Next Connect Successful",
+    })
+}
