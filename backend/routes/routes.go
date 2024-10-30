2ad147ce301ff7c178ed638a63509d0e641c6d73
KENKUN-1031 <kencoding1031@gmail.com>
2024-10-30 13:49:14 +0900

    feat: :sparkles: todoとobjの必要なcontrollerを追加、データのUpdateができるようになった

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -25,6 +25,7 @@ func DefineRoutes(r gin.IRouter) {
 		obj.GET("/getAll", controller.GetAll)
 		obj.GET(":id", controller.GetSingleObj)
 		obj.DELETE("/deleteObj/:id", controller.DeleteObj)
+		obj.PUT("/update/:id", controller.UpdateObj)
 	}
 
 	// AI関連のエンドポイント
@@ -34,5 +35,14 @@ func DefineRoutes(r gin.IRouter) {
 		ai.GET("/goal", controller.GenerateGoalTasks)
 		// 相談内容に対するフィードバック
 		ai.GET("/consultation", controller.ConsultationFeedback)
+	}
+
+	todo := r.Group("/todo")
+	{
+		todo.POST("/", controller.CreateTodo)
+		todo.GET("/getAll", controller.GetTodos)
+		todo.GET("/:id", controller.GetTodo)
+		todo.PUT("/update/:id", controller.UpdateTodo)
+		todo.DELETE("/delete/:id", controller.DeleteTodo)
 	}
 }
